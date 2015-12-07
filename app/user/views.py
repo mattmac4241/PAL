c4f679fbc5b2bc61d645aea55956a8a93dc630d5
mattmac4241 <mattmac4241@yahoo.com>
2015-12-07 10:33:46 -0600

    removed tabs

--- app/user/views.py
+++ app/user/views.py
@@ -7,11 +7,11 @@ users_blueprint = Blueprint('users',__name__)
 @users_blueprint.route("/", methods=['GET', 'POST'])
 def main():
     """Respond to incoming calls with a simple text message."""
- 	from_number = request.values.get('From', None)
- 	user = User.query.filter_by(phone_number=from_number).first()
- 	if user == None:
- 		resp = twilio.twiml.Response()
-        	resp.message("Hello my name is PAL, it is nice to meet you!"
+    from_number = request.values.get('From', None)
+    user = User.query.filter_by(phone_number=from_number).first()
+    if user == None:
+        resp = twilio.twiml.Response()
+            resp.message("Hello my name is PAL, it is nice to meet you!"
         return str(resp)
 
     '''resp = twilio.twiml.Response()
